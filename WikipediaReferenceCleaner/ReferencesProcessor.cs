eeb81e635961c0b2076ca13e4979812d8fc648c8
Björn Larsson <registrering@bjornlarsson.se>
2025-02-06 23:49:17 +0100

    Created main test case.

---
+++ WikipediaReferenceCleaner/ReferencesProcessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikipediaReferenceCleaner
+{
+    public class ReferencesProcessor
+    {
+        public string ProcessLines(IEnumerable<string> lines)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

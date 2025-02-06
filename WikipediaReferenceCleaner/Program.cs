b7ae98991b98a006ae7d89f7c9277e1f33229650
Björn Larsson <registrering@bjornlarsson.se>
2025-02-06 22:57:13 +0100

    Add project files.

---
+++ WikipediaReferenceCleaner/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WikipediaReferenceCleaner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

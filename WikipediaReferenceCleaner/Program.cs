4412a7651285c1baa332078757c08098add9e42d
Björn Larsson <registrering@bjornlarsson.se>
2025-02-07 16:30:51 +0100

    Rewriting main program loop to better show what is happening.

--- WikipediaReferenceCleaner/Program.cs
+++ WikipediaReferenceCleaner/Program.cs
@@ -4,30 +4,43 @@ namespace WikipediaReferenceCleaner
 {
     internal class Program
     {
+        const string Indention = "  ";
+
         static void Main(string[] args)
         {
             var rawReferences = ReadFromInputFile();
 
+            Console.WriteLine("Parsing references...");
             var readReferences = RefListReader.ReadReferences(rawReferences);
+
+            Console.WriteLine("Cleaning up references...");
             var formatedReferences = RefListWriter.ConvertReferencesToString(readReferences);
 
-            var outputFile = WriteToOuputFile(formatedReferences);
+            WriteToOuputFile(formatedReferences);
 
-            Console.WriteLine("The references are processed. The result is found at");
-            Console.WriteLine(outputFile);
+            Console.WriteLine("The references was successfully processed.");
         }
 
         private static string ReadFromInputFile()
         {
             var filepath = GetFilepath("input");
+
+            Console.WriteLine("Reading references from file at");
+            Console.WriteLine(Indention + filepath);
+            Console.WriteLine(Indention + "...");
+
             return File.ReadAllText(filepath);
         }
 
-        private static string WriteToOuputFile(string processedReferences)
+        private static void WriteToOuputFile(string processedReferences)
         {
             var filepath = GetFilepath("output");
+
+            Console.WriteLine("Writing references to file at");
+            Console.WriteLine(Indention + filepath);
+            Console.WriteLine(Indention + "...");
+
             File.WriteAllText(filepath, processedReferences);
-            return filepath;
         }
 
         private static string GetFilepath(string type)

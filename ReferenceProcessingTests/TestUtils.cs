80d90bc95f950177cdeb908c45a5d988d6bbfab4
Björn Larsson <registrering@bjornlarsson.se>
2025-02-08 21:32:02 +0100

    Changing the tests to test the overarcing ReferenceProcessor class instead of the underlying RefListReader and RefListWriter.

--- ReferenceProcessingTests/TestUtils.cs
+++ ReferenceProcessingTests/TestUtils.cs
@@ -15,5 +15,37 @@ namespace ReferenceProcessingTests
             var reader = new StreamReader(filestream);
             return reader.ReadToEnd();
         }
+
+        public static string GetFullPathToTestFile(string filename)
+        {
+            var testProjectName = Assembly.GetCallingAssembly().GetName().Name;
+            var projectFolder = new DirectoryInfo(Environment.CurrentDirectory);
+
+            while (projectFolder.Name != testProjectName)
+            {
+                projectFolder = projectFolder.Parent!;
+            }
+
+            return Path.Combine(projectFolder.FullName, filename);
+        }
+
+        public static string CreateTempFolder()
+        {
+            string tempFolderPath;
+            do
+            {
+                tempFolderPath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+
+            } while (Directory.Exists(tempFolderPath));
+
+            Directory.CreateDirectory(tempFolderPath);
+            return tempFolderPath;
+        }
+
+        public static string CreateTempOutputFilepath(string pathToTempFolder)
+        {
+            var tempFilename = $"{Guid.NewGuid()}-output.txt";
+            return Path.Combine(pathToTempFolder, tempFilename);
+        }
     }
 }

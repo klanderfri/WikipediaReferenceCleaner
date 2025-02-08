3367b7093cbffea126d8d2398450086708ed55e6
Björn Larsson <registrering@bjornlarsson.se>
2025-02-08 18:59:50 +0100

    Extracted code "binding it all together" from the Program class to a separate ReferenceProcessor class.

---
+++ ReferenceProcessing/ReferenceProcessor.cs
@@ -0,0 +1,45 @@
+﻿namespace ReferenceProcessing
+{
+    public class ReferenceProcessor(Messenger messenger)
+    {
+        const string Indention = "  ";
+
+        public void ProcessReferences(string inputFilePath, string outputFilePath)
+        {
+            var rawReferences = ReadFromInputFile(inputFilePath);
+
+            messenger.SendMessage("Parsing references...");
+            var refReader = new RefListReader(messenger);
+            var readSuccessfully = refReader.ReadReferences(rawReferences, out List<Reference> readReferences);
+
+            if (readSuccessfully)
+            {
+                messenger.SendMessage("Cleaning up references...");
+                var refWriter = new RefListWriter(messenger);
+                var formatedReferences = refWriter.ConvertReferencesToString(readReferences);
+
+                WriteToOutputFile(outputFilePath, formatedReferences);
+
+                messenger.SendMessage("The references was successfully processed.");
+            }
+        }
+
+        private string ReadFromInputFile(string inputFilepath)
+        {
+            messenger.SendMessage("Reading references from file at");
+            messenger.SendMessage(Indention + inputFilepath);
+            messenger.SendMessage(Indention + "...");
+
+            return File.ReadAllText(inputFilepath);
+        }
+
+        private void WriteToOutputFile(string outputFilepath, string processedReferences)
+        {
+            messenger.SendMessage("Writing references to file at");
+            messenger.SendMessage(Indention + outputFilepath);
+            messenger.SendMessage(Indention + "...");
+
+            File.WriteAllText(outputFilepath, processedReferences);
+        }
+    }
+}
--- WikipediaReferenceCleaner/Program.cs
+++ WikipediaReferenceCleaner/Program.cs
@@ -4,51 +4,20 @@ namespace WikipediaReferenceCleaner
 {
     internal class Program
     {
-        const string Indention = "  ";
-
         static void Main(string[] args)
         {
             var messanger = new Messenger();
+            var processor = new ReferenceProcessor(messanger);
             messanger.MessageRaised += Messanger_MessageRaised;
 
-            var rawReferences = ReadFromInputFile();
-
-            Console.WriteLine("Parsing references...");
-            var refReader = new RefListReader(messanger);
-            var readSuccessfully = refReader.ReadReferences(rawReferences, out List<Reference> readReferences);
-
-            if (readSuccessfully)
-            {
-                Console.WriteLine("Cleaning up references...");
-                var refWriter = new RefListWriter(messanger);
-                var formatedReferences = refWriter.ConvertReferencesToString(readReferences);
-
-                WriteToOutputFile(formatedReferences);
-
-                Console.WriteLine("The references was successfully processed.");
-            }
+            var inputFilePath = GetFilepath("input");
+            var outputFilePath = GetFilepath("output");
+            processor.ProcessReferences(inputFilePath, outputFilePath);
         }
 
-        private static string ReadFromInputFile()
+        private static void Messanger_MessageRaised(object? sender, EventArgs e)
         {
-            var filepath = GetFilepath("input");
-
-            Console.WriteLine("Reading references from file at");
-            Console.WriteLine(Indention + filepath);
-            Console.WriteLine(Indention + "...");
-
-            return File.ReadAllText(filepath);
-        }
-
-        private static void WriteToOutputFile(string processedReferences)
-        {
-            var filepath = GetFilepath("output");
-
-            Console.WriteLine("Writing references to file at");
-            Console.WriteLine(Indention + filepath);
-            Console.WriteLine(Indention + "...");
-
-            File.WriteAllText(filepath, processedReferences);
+            Console.WriteLine(((MessageArgs)e).Message);
         }
 
         private static string GetFilepath(string type)
@@ -56,11 +25,6 @@ namespace WikipediaReferenceCleaner
             var filename = $"wp-ref-cleaner-{type}.txt";
             var desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
             return Path.Combine(desktop, filename);
-        }
-
-        private static void Messanger_MessageRaised(object? sender, EventArgs e)
-        {
-            Console.WriteLine(((MessageArgs)e).Message);
         }
     }
 }
